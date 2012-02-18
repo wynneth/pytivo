8a81e61f8ffe32eba57e021b7558e1c120bff885
Bruce Goheen <wynn3th@gmail.com>
2012-02-18 03:53:41 -0600

    Added dvdvideo plugin code to httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -291,7 +291,8 @@ class TivoHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                             'Command=NPL&amp;Container=' + quote(section) +  
                             '&amp;TiVo=' + config.tivos[tsn] + '">' + 
                             escape(config.tivo_names[tsn]) + '</a><br>')
-            elif plugin_type == 'video' and t.shares:
+            elif ( plugin_type == 'video' or plugin_type == 'dvdvideo' ) \
+                    and t.shares:
                 t.shares += ('<a href="TiVoConnect?Command=' +
                              'QueryContainer&amp;Container=' +
                              quote(section) + '&Format=text/html">' +
